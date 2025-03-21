c9c595c8456388bd6a4ff7085f121db4da3ee53f
amine9008 <116163022+amine9008@users.noreply.github.com>
2025-03-22 00:33:38 +0100

    Update client.py
    Update code to add queuing logic and regular timestep processing

--- RaspberryPiTestbed/client.py
+++ RaspberryPiTestbed/client.py
@@ -24,7 +24,8 @@ BROKER_IP = "192.168.126.131"
 
 IDLE_POWER = 2.85  # W
 ACTIVE_POWER = 6.00  # W
-INTERVAL_SECONDS = 1.5 # S
+INTERVAL_SECONDS = 1.5 # the timestep length in seconds
+EPISODE_LENGTH = 20 # the episode length in number of timesteps
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -81,63 +82,109 @@ model.to(device)
 part1 = nn.Sequential(*list(model.children())[:-2])
 part2 = model.classifier
 
-image_0_path1 = "D:/Library/copying_to_raspberry/val/Cat/t50.png"
-image_1_path1 = "D:/Library/copying_to_raspberry/val/Dog/g50.png"
+directory_0 = "D:/Library/copying_to_raspberry/val/Cat/t"
+directory_1 = "D:/Library/copying_to_raspberry/val/Dog/g"
 
+directories = [directory_0, directory_1]
 
-while True:
-    image_path = image_0_path1
-    y = 0
-    ACTION_ID = 2 # Action selection
+progressing_list = []
 
-    if ACTION_ID == 0:
-        print("Client offloads the full image")
-        client = mqtt.Client()
-        client.connect(BROKER_IP, 1883, 60)
-        cpu_usage_before = psutil.cpu_percent(interval=0.1) 
-        timestamp_start = time.time()
-        image_str = encode_image(image_path)
-        message = f"{NODE_ID}:{ACTION_ID}:{y}:{timestamp_start}:{image_str}"
-        client.publish(TOPIC, message)
-        duration = (time.time() - timestamp_start)
-        cpu_usage_after = psutil.cpu_percent(interval=0.1)
-        avg_cpu_usage = (cpu_usage_before + cpu_usage_after) / 2
-        energy = estimate_power(duration, avg_cpu_usage)
-        print("client side, data size {}, duration : {} mS, energy consumption {} J".format(len(message), duration * 1000, energy))
-        client.disconnect()
+while True: # For each timestep
+    lambda_ = 2
+    y = np.random.randint(0, 2, size=lambda_)
+    data = np.random.randint(50, 100, size= lambda_)
+    data = data.astype(str)
+    suffices = np.full(lambda_, ".png")
+    all_dirs = np.where(y == 0, directories[0], directories[1])
+    image_paths = all_dirs + data + suffices 
+    
+    ACTION_ID = 3
+    actions = np.full(lambda_, ACTION_ID)
 
-    elif ACTION_ID == 1:
-        print("Client compresses and offloads the image")
-        client = mqtt.Client()
-        client.connect(BROKER_IP, 1883, 60)
-        cpu_usage_before = psutil.cpu_percent(interval=0.1) 
-        timestamp_start = time.time()
-        image_str = resize_and_compress_image(image_path, 112, 112, compression_level=9)
-        message = f"{NODE_ID}:{ACTION_ID}:{y}:{timestamp_start}:{image_str}"
-        client.publish(TOPIC, message)
-        duration = (time.time() - timestamp_start)
-        cpu_usage_after = psutil.cpu_percent(interval=0.1)
-        avg_cpu_usage = (cpu_usage_before + cpu_usage_after) / 2
-        energy = estimate_power(duration, avg_cpu_usage)
-        print("client side, data size {}, duration : {} mS, energy consumption {} J".format(len(message), duration * 1000, energy))
-        client.disconnect()
+    tuple_array = np.array(list(zip(actions, image_paths)))
+    
+    #progressing_list = np.concatenate((tuple_array,tuple_array2))
+    if len(progressing_list) == 0: 
+        progressing_list = tuple_array
+    else:
+        progressing_list = np.concatenate((progressing_list,tuple_array))
 
-    elif ACTION_ID == 2:
-        print("Client Partial Offload and send intermediate")
-        client = mqtt.Client()
-        client.connect(BROKER_IP, 1883, 60)
-        cpu_usage_before = psutil.cpu_percent(interval=0.1) 
-        timestamp_start = time.time()
-        image_tensor = preprocess_image(image_path)
-        tensor = part1(image_tensor)
-        tensor_str = send_intermediate_tensor(tensor)
-        message = f"{NODE_ID}:{ACTION_ID}:{y}:{timestamp_start}:{tensor_str}"
-        client.publish(TOPIC, message)
-        duration = (time.time() - timestamp_start)
-        cpu_usage_after = psutil.cpu_percent(interval=0.1)
-        avg_cpu_usage = (cpu_usage_before + cpu_usage_after) / 2
-        energy = estimate_power(duration, avg_cpu_usage)
-        print("client side, data size {}, duration : {} mS, energy consumption {} J".format(len(message), duration * 1000, energy))
-        client.disconnect()
+    print(progressing_list)
+    total_elapsed = 0.0
+    #for (image_path, ACTION_ID) in progressing_list:
+    for [action_id, image_path] in progressing_list:
+        duration = 0
+        if ACTION_ID == 0:
+            print("Client offloads the full image")
+            client = mqtt.Client()
+            client.connect(BROKER_IP, 1883, 60)
+            cpu_usage_before = psutil.cpu_percent(interval=0.1) 
+            timestamp_start = time.time()
+            image_str = encode_image(image_path)
+            message = f"{NODE_ID}:{ACTION_ID}:{y}:{timestamp_start}:{image_str}"
+            client.publish(TOPIC, message)
+            duration = (time.time() - timestamp_start)
+            cpu_usage_after = psutil.cpu_percent(interval=0.1)
+            avg_cpu_usage = (cpu_usage_before + cpu_usage_after) / 2
+            energy = estimate_power(duration, avg_cpu_usage)
+            print("client side, data size {}, duration : {} mS, energy consumption {} J".format(len(message), duration * 1000, energy))
+            client.disconnect()
+
+        elif ACTION_ID == 1:
+            print("Client compresses and offloads the image")
+            client = mqtt.Client()
+            client.connect(BROKER_IP, 1883, 60)
+            cpu_usage_before = psutil.cpu_percent(interval=0.1) 
+            timestamp_start = time.time()
+            image_str = resize_and_compress_image(image_path, 112, 112, compression_level=9)
+            message = f"{NODE_ID}:{ACTION_ID}:{y}:{timestamp_start}:{image_str}"
+            client.publish(TOPIC, message)
+            duration = (time.time() - timestamp_start)
+            cpu_usage_after = psutil.cpu_percent(interval=0.1)
+            avg_cpu_usage = (cpu_usage_before + cpu_usage_after) / 2
+            energy = estimate_power(duration, avg_cpu_usage)
+            print("client side, data size {}, duration : {} mS, energy consumption {} J".format(len(message), duration * 1000, energy))
+            client.disconnect()
+
+        elif ACTION_ID == 2:
+            print("Client Partial Offload and send intermediate")
+            client = mqtt.Client()
+            client.connect(BROKER_IP, 1883, 60)
+            cpu_usage_before = psutil.cpu_percent(interval=0.1) 
+            timestamp_start = time.time()
+            image_tensor = preprocess_image(image_path)
+            tensor = part1(image_tensor)
+            tensor_str = send_intermediate_tensor(tensor)
+            message = f"{NODE_ID}:{ACTION_ID}:{y}:{timestamp_start}:{tensor_str}"
+            client.publish(TOPIC, message)
+            duration = (time.time() - timestamp_start)
+            cpu_usage_after = psutil.cpu_percent(interval=0.1)
+            avg_cpu_usage = (cpu_usage_before + cpu_usage_after) / 2
+            energy = estimate_power(duration, avg_cpu_usage)
+            print("client side, data size {}, duration : {} mS, energy consumption {} J".format(len(message), duration * 1000, energy))
+            client.disconnect()
+        
+        elif ACTION_ID == 3:
+            print("Client performs Total inference")
+            client = mqtt.Client()
+            client.connect(BROKER_IP, 1883, 60)
+            cpu_usage_before = psutil.cpu_percent(interval=0.1) 
+            timestamp_start = time.time()
+            image_tensor = preprocess_image(image_path)
+            result = model(image_tensor)
+            tensor_str = send_intermediate_tensor(result)
+            message = f"{NODE_ID}:{ACTION_ID}:{y}:{timestamp_start}:{tensor_str}"
+            client.publish(TOPIC, message)
+            duration = (time.time() - timestamp_start)
+            cpu_usage_after = psutil.cpu_percent(interval=0.1)
+            avg_cpu_usage = (cpu_usage_before + cpu_usage_after) / 2
+            energy = estimate_power(duration, avg_cpu_usage)
+            print("client side, data size {}, duration : {} mS, energy consumption {} J".format(len(message), duration * 1000, energy))
+            client.disconnect()
+        total_elapsed = total_elapsed + duration
+        progressing_list = np.delete(progressing_list, 0, axis=0)
+        if(total_elapsed >= INTERVAL_SECONDS):
+            break
+
     sleep_time = max(0, INTERVAL_SECONDS - total_elapsed)
     time.sleep(sleep_time)
